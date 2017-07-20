70bb619a1a4b53ffaee23738c4e07bea31202e32
uf0 <scnepa222@gmail.com>
2017-07-20 17:33:33 +0200

    bootstrap project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hdilApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hdilApp
+ */
+angular.module('hdilApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
