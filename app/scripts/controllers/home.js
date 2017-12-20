7859ed963548bf574091f8d47770d9eed1adeb89
giorgio <giorgio@calib.ro>
2017-12-20 23:20:17 +0100

    added content

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -9,6 +9,6 @@
  */
 angular.module('hdilApp')
   .controller('HomeCtrl', function ($scope) {
-    $scope.title = "Human data interaction<br>in Lombardia";
+    $scope.title = "Valore sociale della visualizzazione di open data di Regione Lombardia";
     $scope.description = "Lorem ipsum dolor sit amet, vim laudem accommodare te. Mea an constituto honestatis referrentur, eros philosophia vim ei. Qui in esse oportere, etiam postea quaestio te eos. Quo ut cibo ludus iracundia, ea iudico maiorum invenire eum. Vocent voluptatum at vel, et modus ignota commune has, dico splendide ut mel. In eripuit recusabo eum.";
   });
